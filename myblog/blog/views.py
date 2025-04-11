3af043303383d21b9c6b897f522e47f791e179f6
Francisca <francisca.paredes.pr@gmail.com>
2025-04-11 09:32:07 -0400

    send mail

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -27,7 +27,9 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
     sent = False
+
     if request.method == 'POST':
         # form was submitteds
         form = EmailPostForm(request.POST)
@@ -37,11 +39,11 @@ def post_share(request, post_id):
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = (
                 f"{cd['name']} ({cd['email']})"
-                f"recommends you read {post.title}"
+                f" recommends you read {post.title}"
             )
             message = (
                 f"Read {post.title} at {post_url}\n\n"
-                f"{cd['name']}\'s comments: {cd['comments']}"
+                f"{cd['name']}'s comments: {cd['comments']}"
             )
             send_mail(
                 subject=subject,
@@ -52,10 +54,10 @@ def post_share(request, post_id):
             sent = True
     else:
         form = EmailPostForm()
-        return render(
-            request, 'blog/post/share.html', {
-                'post': post,
-                'form': form,
-                'sent': sent
-            }
-        )
+    return render(
+        request, 'blog/post/share.html', {
+            'post': post,
+            'form': form,
+            'sent': sent
+        }
+    )
